9b95b9e113e05cfe85fb925e1360aa2346f026e8
a4802470 <121592205+a4802470@users.noreply.github.com>
2023-05-25 21:11:32 +0800

    update

---
+++ Assets/Scfipts/menumanagers.cs
@@ -0,0 +1,20 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class menumanagers : MonoBehaviour
+{
+    public void Startgmae()
+    {
+        SceneManager.LoadScene("遊戲場景");
+    }
+
+    public void Quitgmae()
+    {
+        Application.Quit();
+    }
+
+
+
+
+
+}
